222ff8f83841a0a08a02870b51d42eb7a49ba4de
Joris Falter <joris@jorisfalter.com>
2025-03-28 14:52:12 +0100

    x

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -6,49 +6,48 @@ if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
 }
 
-// Add proper type declaration for global mongoose
-declare global {
-  // eslint-disable-next-line no-var
-  var mongoose: {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
-  } | undefined;
-}
-
-interface Cached {
+interface MongooseCache {
   conn: typeof mongoose | null;
   promise: Promise<typeof mongoose> | null;
 }
 
-const cached: Cached = global.mongoose || { conn: null, promise: null };
+// Add proper type declaration for global mongoose
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseCache;
+}
 
+// Initialize the cached connection object
 if (!global.mongoose) {
-  global.mongoose = cached;
+  global.mongoose = {
+    conn: null,
+    promise: null
+  };
 }
 
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
+  if (global.mongoose.conn) {
+    return global.mongoose.conn;
   }
 
-  if (!cached.promise) {
+  if (!global.mongoose.promise) {
     const opts = {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+    global.mongoose.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
     });
   }
 
   try {
-    cached.conn = await cached.promise;
+    global.mongoose.conn = await global.mongoose.promise;
   } catch (e) {
-    cached.promise = null;
+    global.mongoose.promise = null;
     throw e;
   }
 
-  return cached.conn;
+  return global.mongoose.conn;
 }
 
 export default dbConnect; 
